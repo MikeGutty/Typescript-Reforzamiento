942a6698fc1c1c52f1ecfd7637b61daca1e8819f
Mike <michael777gutty@gmail.com>
2026-04-14 15:43:55 -0400

    first commit

---
+++ src/bases/template-strings.ts
@@ -0,0 +1,6 @@
+const firstName = 'Fernando';
+const lastName = "O'Neal";
+
+const fullName = `Hola mundo ${firstName} ${lastName}`;
+
+console.log(fullName);
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+// import './bases/const-let.ts'
+// import './bases/template-strings.ts'
+// import './bases/object-literal';
+// import './bases/arrays';
+// import './bases/functions';
+// import './bases/obj-destructuring';
+// import './bases/array-destructuring';
+// import './bases/imp-exp';
+// import './bases/promises';
+// import './bases/fetch-api';
+import './bases/async-await';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
